e25c7adbe635be10a2935a5b8cadd02d8342fdc8
Erick Patrick <erick.marinho2003@gmail.com>
2025-08-04 22:58:00 -0300

    demonstration of what pointers are and how they work in go

---
+++ user-input-and-pointers/user-input-and-pointers.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+	// Print the memory location
+	// fmt.Println(&conferenceName)
+
+	fmt.Println("----------------------------------------")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// Ask user for their name
+	// '&' = Address-of operator
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	// It needs to be the same type (uint - uint)
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining", remainingTickets)
+}
